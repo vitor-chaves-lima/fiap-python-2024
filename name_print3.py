e0ce88fa089c29507436970925862d7a754cde4c
Vitor Coelho <vitor.coelho@skf.com>
2024-03-22 21:36:16 -0300

    Add name_print2, name_print3 and sum_print code

---
+++ name_print3.py
@@ -0,0 +1,4 @@
+name: str = input("Digite seu nome: ")
+age: str = input("Digite sua idade: ")
+
+print(f"Olá meu nome é {name} e tenho {age} anos")
---
+++ sum_print.py
@@ -0,0 +1,5 @@
+number_1: int = int(input("Digite o número 1: "))
+number_2: int = int(input("Digite o número 2: "))
+
+number_sum: int = number_1 + number_2
+print(f"Soma: {number_sum}")
