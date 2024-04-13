5c46f7e8a666aed4cc63b62713aebce8b8e9b4f6
Vitor Coelho <vitor.coelho@skf.com>
2024-04-12 22:47:55 -0300

    Update while loop code

--- while_loop.py
+++ while_loop.py
@@ -1,5 +1,20 @@
 
+# num = 1
+# while num < 100:
+#     num += 1
+#     print(num)
+
+
+# num = 50
+# while num < 100:
+#     num += 1
+#     print(num)
+
 num = 1
 while num < 100:
     num += 1
-    print(num)
+
+    if(num % 2 == 0):
+        print(num, "par")
+    else:
+        print(num, "impar")
