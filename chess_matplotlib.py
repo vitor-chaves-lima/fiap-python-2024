52dfdd0d7a96b5aee11fb4d6ee02839bda4f709a
Vitor Coelho <vitor.coelho@skf.com>
2024-09-13 21:52:57 -0300

    Optimizing chess_matplotlib code

--- chess_matplotlib.py
+++ chess_matplotlib.py
@@ -3,20 +3,7 @@ import matplotlib.pyplot as plt
 WHITE_COLOR = [255, 255,255]
 BLACK_COLOR = [0, 0, 0]
 
-chess_matrix = [[[None,None,None] for _ in range(8)] for _ in range(8)]
-
-for iR, r in enumerate(chess_matrix):
-    for iC, _ in enumerate(r):
-        if iR % 2 == 0:
-            if iC % 2 == 0:
-                chess_matrix[iR][iC] = WHITE_COLOR
-            else:
-                chess_matrix[iR][iC] = BLACK_COLOR
-        else:
-            if iC % 2 == 0:
-                chess_matrix[iR][iC] = BLACK_COLOR
-            else:
-                chess_matrix[iR][iC] = WHITE_COLOR
+chess_matrix = [[[255*((i+j)%2),255*((i+j)%2),255*((i+j)%2)] for i in range(8)] for j in range(8)]
 
 plt.imshow(chess_matrix)
 plt.axis('off')
